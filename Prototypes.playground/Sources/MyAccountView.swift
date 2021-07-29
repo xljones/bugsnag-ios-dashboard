beefb910fe6944d8d1b21bd79bdd4d031aac842d
Xander Jones <xander@bugsnag.com>
2021-07-29 15:09:21 +0100

    add programatic dismiss btn in account view

--- Prototypes.playground/Sources/MyAccountView.swift
+++ Prototypes.playground/Sources/MyAccountView.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 
 public struct MyAccountView: View {
+    @Environment(\.presentationMode) var thisViewActive
+    
     public init() {
     }
+    
     public var body: some View {
         Text("My Account details go here")
+        Button("Dismiss") {
+            self.thisViewActive.wrappedValue.dismiss()
+        }
     }
 }
