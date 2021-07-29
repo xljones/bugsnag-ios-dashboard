3a1227d0cd9a3551a02af4d4a20657b8427b5e3b
Xander Jones <xander@bugsnag.com>
2021-07-29 15:08:46 +0100

    add more colors

--- Prototypes.playground/Sources/Colors.swift
+++ Prototypes.playground/Sources/Colors.swift
@@ -15,3 +15,17 @@ public struct BSGSecondaryColors {
     public static let coral = Color(red: 255/255, green: 90/255, blue: 96/255)
     public static let green = Color(red: 140/255, green: 198/255, blue: 63/255)
 }
+
+// Bugsnag extended palette color scheme
+public struct BSGExtendedColors {
+    public static let batman00 = Color(red: 248/255, green: 248/255, blue: 248/255)
+    public static let batman10 = Color(red: 233/255, green: 233/255, blue: 233/255)
+    public static let batman20 = Color(red: 198/255, green: 198/255, blue: 198/255)
+    public static let batman30 = Color(red: 174/255, green: 174/255, blue: 174/255)
+    public static let batman40 = Color(red: 149/255, green: 149/255, blue: 149/255)
+    public static let batman50 = Color(red: 124/255, green: 124/255, blue: 124/255)
+    public static let batman60 = Color(red: 99/255, green: 99/255, blue: 99/255)
+    public static let batman70 = Color(red: 74/255, green: 74/255, blue: 74/255)
+    public static let batman80 = Color(red: 50/255, green: 50/255, blue: 50/255)
+    public static let batman90 = Color(red: 33/255, green: 33/255, blue: 33/255)
+}
