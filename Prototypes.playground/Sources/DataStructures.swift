c22503f0fa269588986d3eb492105baba85d9217
Xander Jones <xander@bugsnag.com>
2021-07-29 15:08:40 +0100

    expand data structures with initializers

--- Prototypes.playground/Sources/DataStructures.swift
+++ Prototypes.playground/Sources/DataStructures.swift
@@ -1,4 +1,41 @@
+public struct BSGOrganization {
+    public init(id: String,
+                name: String){
+        self.id = id
+        self.name = name
+    }
+    var id: String
+    var name: String
+}
+
+public struct BSGProject {
+    public init(id: String, 
+                name: String,
+                type: String) {
+        self.id = id
+        self.name = name
+        self.type = type
+    }
+    var id: String
+    var name: String
+    var type: String
+}
+
 public struct BSGError {
+    public init(id: String,
+                errorClass: String, 
+                errorMessage: String,
+                errorContext: String,
+                userCount: Int,
+                eventCount: Int) {
+        self.id = id
+        self.errorClass = errorClass
+        self.errorMessage = errorMessage
+        self.errorContext = errorContext
+        self.userCount = userCount
+        self.eventCount = eventCount
+    }
+    var id: String
     var errorClass: String
     var errorMessage: String
     var errorContext: String
@@ -7,6 +44,16 @@ public struct BSGError {
 }
 
 public struct BSGEvent {
+    public init(id: String,
+                errorClass: String,
+                errorMessage: String,
+                errorContext: String) {
+        self.id = id
+        self.errorClass = errorClass
+        self.errorMessage = errorMessage
+        self.errorContext = errorContext
+    }
+    var id: String
     var errorClass: String
     var errorMessage: String
     var errorContext: String
