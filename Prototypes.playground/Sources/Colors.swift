adcd54271d66be4cc80dd3e7689829896da36a04
Xander Jones <xander@bugsnag.com>
2021-07-29 11:35:02 +0100

    add color palette

---
+++ Prototypes.playground/Sources/Colors.swift
@@ -0,0 +1,17 @@
+
+import SwiftUI
+
+// Bugsnag Primary color scheme
+public struct BSGPrimaryColors {
+    public static let indigo = Color(red: 73/255, green: 73/255, blue: 228/255)
+    public static let midnight = Color(red: 0/255, green: 13/255, blue: 71/255)
+}
+
+// Bugsnag Secondary color scheme
+public struct BSGSecondaryColors {
+    public static let pacific = Color(red: 55/255, green: 194/255, blue: 217/255)
+    public static let sunflower = Color(red: 255/255, green: 163/255, blue: 0/255)
+    public static let orchid = Color(red: 161/255, green: 95/255, blue: 255/255)
+    public static let coral = Color(red: 255/255, green: 90/255, blue: 96/255)
+    public static let green = Color(red: 140/255, green: 198/255, blue: 63/255)
+}
