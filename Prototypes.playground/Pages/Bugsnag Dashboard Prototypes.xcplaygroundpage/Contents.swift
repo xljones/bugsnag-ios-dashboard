dcb6f2f0819fa85ee250b24fdbd0adc48d80682b
Xander Jones <xander@bugsnag.com>
2021-07-29 15:09:30 +0100

    main contents update

--- Prototypes.playground/Pages/Bugsnag Dashboard Prototypes.xcplaygroundpage/Contents.swift
+++ Prototypes.playground/Pages/Bugsnag Dashboard Prototypes.xcplaygroundpage/Contents.swift
@@ -2,22 +2,56 @@
 import SwiftUI
 import PlaygroundSupport
 
-let bugsnagDAAToken: String =  "MY_TOKEN"
-
-struct BugsnagTitle: View{
+// Title View contains the title text, project context menu button and account context menu button
+struct BugsnagTitle: View {
+    @State private var showingMyAccountView: Bool = false
+    @State private var showingProjectSelectorView: Bool = false
+    
     var body: some View {
-        Text("Bugsnag")
-            .padding(25)
-            .frame(maxWidth: .infinity, alignment: .center)
-            .foregroundColor(.white)
-            .background(BSGPrimaryColors.midnight)
-            .font(.title)
+        // A Stack to present a background color
+        ZStack() {
+            // A horizontal stack to contain the hamburger, title, and account buttons
+            HStack() {
+                Divider()
+                Button(action: {
+                    print("Open the project menu") 
+                    self.showingProjectSelectorView.toggle()
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .font(.system(size: 30))
+                        .foregroundColor(.white)
+                }
+                Text("Bugsnag")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .foregroundColor(.white)
+                    .font(.title)
+                Button(action: {
+                    self.showingMyAccountView.toggle()
+                }) {
+                    Image(systemName: "person.crop.circle")
+                        .font(.system(size: 30))
+                        .foregroundColor(.white)
+                }
+                Divider()
+            }.frame(width: .infinity, height: 70, alignment: .center)
+        }.background(BSGPrimaryColors.midnight)
+        
+        // When Account view is toggled, show this sheet.
+        .sheet(isPresented: $showingMyAccountView) {
+            MyAccountView()
+        }
     }
 }
 
 // The core view of the application
-struct CoreView: View {
-    init() { }
+struct CoreView: View { 
+    
+    let bugsnagDAAToken: String =  "MY_TOKEN"
+    var organization = BSGOrganization.init(id: "abcdef1234556788", name: "My Org")
+    var projects = [
+        BSGProject.init(id: UUID().uuidString, name: "Android", type: "android"),
+        BSGProject.init(id: UUID().uuidString, name: "iOS", type: "ios")
+    ]
     
     var body: some View {
         BugsnagTitle()
@@ -30,11 +64,46 @@ struct CoreView: View {
                 .tabItem {
                     Label("Timeline", systemImage:"chart.bar.xaxis")
                 }
-            MyAccountView()
-                .tabItem {
-                    Label("My Account", systemImage:"person.crop.circle")
-                }
+        }.accentColor(BSGSecondaryColors.coral)
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            Spacer()
         }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
