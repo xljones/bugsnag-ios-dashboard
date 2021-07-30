25f1582afb4da0d119a6fb09fec337c5a446aa62
Xander Jones <xander@bugsnag.com>
2021-07-30 14:04:21 +0100

    add token class & re-arrange structs

--- BugsnagDashboard/BugsnagDashboard/Data Structures/Organization.swift
+++ BugsnagDashboard/BugsnagDashboard/Data Structures/Organization.swift
@@ -6,17 +6,42 @@
 //
 
 import Foundation
-public struct BSGOrganization {
-    public init(id: String,
-                name: String){
-        self.id = id
-        self.name = name
+
+public class BSGToken {
+    private var token: String
+    
+    public init(token: String) {
+        self.token = token
     }
+    
+    public func setToken(token: String) {
+        self.token = token
+    }
+    
+    public func getToken() -> String {
+        return self.token
+    }
+}
+
+public struct BSGOrganization {
     var id: String
     var name: String
+    var slug: String
+    
+    public init(id: String,
+                name: String,
+                slug: String) {
+        self.id = id
+        self.name = name
+        self.slug = slug
+    }
 }
 
 public struct BSGProject {
+    var id: String
+    var name: String
+    var type: String
+    
     public init(id: String,
                 name: String,
                 type: String) {
@@ -24,7 +49,4 @@ public struct BSGProject {
         self.name = name
         self.type = type
     }
-    var id: String
-    var name: String
-    var type: String
 }
