c8389b143d9ffae75c300bbeac1522a12594b7ea
Xander Jones <xander@bugsnag.com>
2021-12-09 15:45:49 +0000

    getOrganizations API implementation

--- BugsnagDashboard/BugsnagDashboard/Libraries/API/Organizations.swift
+++ BugsnagDashboard/BugsnagDashboard/Libraries/API/Organizations.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public func getOrganizations(token: BSGToken, completionHandler: @escaping (Result<[BSGOrganization], Error>) -> Void) {
+    var request = URLRequest(url: URL(string: "https://api.bugsnag.com/user/organizations")!)
+    request.httpMethod = "GET"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("token \(token.getToken())", forHTTPHeaderField: "Authorization")
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data else { return completionHandler(.failure(error!)) }
+        do {
+            let orgs = try JSONDecoder().decode([BSGOrganization].self, from: data)
+            completionHandler(.success(orgs))
+        } catch {
+            completionHandler(.failure(error))
+        }
+    }.resume()
+}
