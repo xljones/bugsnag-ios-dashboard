08a7866c0c8d29e5854167ebcc6240b603a99e3f
Xander Jones <xander@bugsnag.com>
2021-12-15 15:23:13 +0000

    add project struct

---
+++ BugsnagDashboard/BugsnagDashboard/Data Structures/StructProject.swift
@@ -0,0 +1,26 @@
+//
+//  StructProject.swift
+//  BugsnagDashboard
+//
+//  Created by Xander Jones on 15/12/2021.
+//
+
+import Foundation
+
+// MARK: - BSGProject
+public struct BSGProject: Codable {
+    let id, organizationID, slug, name: String
+    let apiKey, type: String
+    let isFullView: Bool
+    let releaseStages: [String]
+    let language: String
+    let createdAt, updatedAt: String
+    let errorsURL, eventsURL, url, htmlURL: String
+    let openErrorCount, forReviewErrorCount, collaboratorsCount: Int
+    let globalGrouping, locationGrouping, discardedAppVersions, discardedErrors: [String]?
+    let customEventFieldsUsed: Int
+    let resolveOnDeploy: Bool
+    let urlWhitelist: [String]?
+    let ignoreOldBrowsers: Bool?
+    let ignoredBrowserVersions: [String]?
+}
