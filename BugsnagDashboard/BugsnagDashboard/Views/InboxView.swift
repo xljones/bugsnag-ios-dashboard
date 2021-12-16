d8e3e99dd82b53c3006d3468720d962590e0b0ad
Xander Jones <xander@bugsnag.com>
2021-12-16 18:20:29 +0000

    show latest error on inbox view

--- BugsnagDashboard/BugsnagDashboard/Views/InboxView.swift
+++ BugsnagDashboard/BugsnagDashboard/Views/InboxView.swift
@@ -17,61 +17,63 @@ public struct InboxView: View {
     }
     
     public var body: some View {
-        VStack(alignment: .leading) {
-            if let project = activeProject {
-                Text(project.details.name)
+        VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Inbox")
                     .font(.title)
-                    .padding(.horizontal, 20.0)
-                    .padding(.top, 10.0)
-                errorListContainerView
+                if let project = activeProject {
+                    Text(project.details.name)
+                        .font(.footnote)
+                }
+            }.padding(20)
+            Divider()
+            VStack(alignment: .leading) {
+                List {
+                    Section(header: Text("Latest errors")) {
+                        if let errs = latestErrors {
+                            if (errs.count == 0) {
+                                Text("No errors on this project")
+                                    .foregroundColor(BSGExtendedColors.batman40)
+                            } else {
+                                ForEach(errs, id: \.id) { err in
+                                    VStack(alignment: .leading) {
+                                        Text("class").foregroundColor(BSGExtendedColors.batman40).font(.system(size:10))
+                                        Text(err.errorClass)
+                                        Text("message").foregroundColor(BSGExtendedColors.batman40).font(.system(size:10))
+                                        Text(err.message)
+                                        Text("context").foregroundColor(BSGExtendedColors.batman40).font(.system(size:10))
+                                        Text(err.context)
+                                    }
+                                }
+                            }
+                        } else {
+                            Text("No error information available.")
+                                .foregroundColor(BSGExtendedColors.batman40)
+                        }
+                    }
+                }
             }
-        }
-        .padding(0)
-        .onAppear {
+            .onAppear {
                 if let project = activeProject {
                     getErrors(token: myToken, project: project.details) {
                         switch $0 {
                         case let .success(rtnErrors):
                             latestErrors = rtnErrors
+                            print(latestErrors)
                             print("getErrors Success (on appear)")
                         case let .failure(error):
                             print("getProjects Failed (on appear): \(error)")
                         }
                     }
                 }
-        }
-    }
-    
-    @ViewBuilder
-    public var errorListContainerView: some View {
-        if let _ = latestErrors {
-            errorListView
-        } else {
-            emptyErrorListView
-        }
-    }
-    
-    var emptyErrorListView: some View {
-        Text("No errors.")
-    }
-    
-    var errorListView: some View {
-        List(latestErrors!, id: \.id) { err in
-            VStack(alignment: .leading) {
-                HStack {
-                    Text(err.errorClass)
-                        .bold()
-                    Text(err.context)
-                }
-                Text(err.message)
-                    .foregroundColor(BSGExtendedColors.batman30)
             }
         }
     }
 }
 
 struct InboxView_Previews: PreviewProvider {
-    @State static var testActiveProject: ActiveProject?
+    static var testProject = BSGProject.init(id: "613a095c6e7316000ef78276", organizationID: "6113b207304caa000e308c2a", slug: "test-project", name: "My Test Project", apiKey: "97ef27a04c69ae72307ba2a3b7168b5b", type: "iOS", isFullView: true, releaseStages: ["production", "development"], language: "language", createdAt: "2021-12-01 12:00:00", updatedAt: "2021-12-01 12:00:00", errorsURL: "https://errorsURL.com", eventsURL: "https://eventsURL.com", url: "https://URL.com", htmlURL: "https://htmlURL.com", openErrorCount: 190, forReviewErrorCount: 25, collaboratorsCount: 4, globalGrouping: [], locationGrouping: [], discardedAppVersions: [], discardedErrors: [], customEventFieldsUsed: 2, resolveOnDeploy: false, urlWhitelist: [], ignoreOldBrowsers: false)
+    @State static var testActiveProject: ActiveProject? = ActiveProject.init(index: 1, details: testProject)
     static var previews: some View {
         InboxView(activeProject: $testActiveProject)
     }
