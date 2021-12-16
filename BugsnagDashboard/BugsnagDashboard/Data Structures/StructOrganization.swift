ea33b71b40b163d0b528560f5253caec6c45afe0
Xander Jones <xander@bugsnag.com>
2021-12-16 16:29:10 +0000

    add coding keys to organization struct

--- BugsnagDashboard/BugsnagDashboard/Data Structures/StructOrganization.swift
+++ BugsnagDashboard/BugsnagDashboard/Data Structures/StructOrganization.swift
@@ -7,28 +7,35 @@
 
 import Foundation
 
-// MARK: - Organizations
-// MARK: Present: Swift is very strict on typing to ensure that without checking, you can never present a nil value
+// MARK: - BSGOrganization
 public struct BSGOrganization: Codable {
-    var id, name, slug: String
-    var creator: BSGOrganizationCreator?
-    var collaboratorsURL, projectsURL: String?
-    var createdAt, updatedAt: String?
-    var autoUpgrade: Bool?
-    var upgradeURL: String?
-    var canStartProTrial: Bool?
-    var proTrialEndsAt: String?
-    var proTrialFeature: Bool?
-    var billingEmails: [String]?
-    
-    init() {
-        self.id = ""
-        self.name = ""
-        self.slug = ""
+    let id, name, slug: String
+    let creator: BSGOrganizationCreator
+    let collaboratorsURL, projectsURL: String
+    let createdAt, updatedAt: String
+    let autoUpgrade: Bool
+    let upgradeURL: String
+    let canStartProTrial: Bool
+    let proTrialEndsAt: String?
+    let proTrialFeature: Bool
+    let billingEmails: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, slug, creator
+        case collaboratorsURL = "collaborators_url"
+        case projectsURL = "projects_url"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+        case autoUpgrade = "auto_upgrade"
+        case upgradeURL = "upgrade_url"
+        case canStartProTrial = "can_start_pro_trial"
+        case proTrialEndsAt = "pro_trial_ends_at"
+        case proTrialFeature = "pro_trial_feature"
+        case billingEmails = "billing_emails"
     }
 }
 
 // MARK: BSGOrganizationCreator
 struct BSGOrganizationCreator: Codable {
-    var id, name, email: String?
+    var id, name, email: String
 }
