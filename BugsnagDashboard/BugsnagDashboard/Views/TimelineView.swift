c73cdf76200a3a670ffab8b19dc383987e9388db
Xander Jones <xander@bugsnag.com>
2021-12-18 22:28:30 +0000

    add timeline view harness

--- BugsnagDashboard/BugsnagDashboard/Views/TimelineView.swift
+++ BugsnagDashboard/BugsnagDashboard/Views/TimelineView.swift
@@ -9,25 +9,31 @@ import Foundation
 import SwiftUI
 
 public struct TimelineView: View {
-    public init() {
+    @Binding var activeProject: ActiveProject?
+    
+    public init(activeProject: Binding<ActiveProject?>) {
+        _activeProject = activeProject
     }
     
     public var body: some View {
-        VStack(alignment: .leading) {
-            Text("Project Name")
-                .font(.title)
-                .padding(.horizontal, 20.0)
-                .padding(.top, 10.0)
+        VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Timeline")
+                    .font(.title)
+                if let project = activeProject {
+                    Text(project.details.name)
+                        .font(.footnote)
+                }
+            }.padding(20)
             Divider()
-                .frame(height:1)
-                .background(BSGSecondaryColors.coral)
-            Text("Timeline Goes here")
         }
     }
 }
 
 struct TimelineView_Previews: PreviewProvider {
+    static var testProject = BSGProject.init(id: "613a095c6e7316000ef78276", organizationID: "6113b207304caa000e308c2a", slug: "test-project", name: "My Test Project", apiKey: "97ef27a04c69ae72307ba2a3b7168b5b", type: "iOS", isFullView: true, releaseStages: ["production", "development"], language: "language", createdAt: "2021-12-01 12:00:00", updatedAt: "2021-12-01 12:00:00", errorsURL: "https://errorsURL.com", eventsURL: "https://eventsURL.com", url: "https://URL.com", htmlURL: "https://htmlURL.com", openErrorCount: 190, forReviewErrorCount: 25, collaboratorsCount: 4, globalGrouping: [], locationGrouping: [], discardedAppVersions: [], discardedErrors: [], customEventFieldsUsed: 2, resolveOnDeploy: false, urlWhitelist: [], ignoreOldBrowsers: false)
+    @State static var testActiveProject: ActiveProject? = ActiveProject.init(index: 1, details: testProject)
     static var previews: some View {
-        TimelineView()
+        TimelineView(activeProject: $testActiveProject)
     }
 }
