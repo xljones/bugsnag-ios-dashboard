a30532dd07eea86065702e7948e6ad4223fdf920
Xander Jones <xander@bugsnag.com>
2021-12-21 00:57:10 +0000

    releases api placeholder

---
+++ BugsnagDashboard/BugsnagDashboard/Libraries/API/APIReleases.swift
@@ -0,0 +1,11 @@
+//
+//  APIReleases.swift
+//  BugsnagDashboard
+//
+//  Created by Xander Jones on 21/12/2021.
+//
+
+import Foundation
+
+// GEThttps://api.bugsnag.com/projects/project_id/releases?release_stage=production&base=2018-02-22T12%3A00%3A00Z&sort=timestamp&offset=0&per_page=5
+
